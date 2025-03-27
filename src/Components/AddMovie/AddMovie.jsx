55dda194a071aa12ddd1529165a7bb021826fb42
FatmaLajmi <lajmifatma10@gmail.com>
2025-03-27 17:28:53 +0100

    Second commit

--- src/Components/AddMovie/AddMovie.jsx
+++ src/Components/AddMovie/AddMovie.jsx
@@ -1,19 +1,24 @@
-import React, { useRef } from "react";
+import React, { useRef, useState} from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import './AddMovie.css';
+import Modal from 'react-bootstrap/Modal';
 
 const AddMovie = ({ filteredMovies, setFilteredMovies }) => {
   const titleRef = useRef();
   const descRef = useRef();
   const ratingRef = useRef();
   const posterRef = useRef();
-
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  
   const handleAddMovie = () => {
     const title = titleRef.current.value.trim();
     const description = descRef.current.value.trim();
     const rating = parseFloat(ratingRef.current.value);
     const posterUrl = posterRef.current.value.trim();
+    
 
     if (!title || !description || isNaN(rating) || !posterUrl) {
       alert("Please fill in all fields correctly.");
@@ -37,13 +42,38 @@ const AddMovie = ({ filteredMovies, setFilteredMovies }) => {
   };
 
   return (
-    <Form className="add-movie-form">
-      <Form.Control type="text" placeholder="Title" ref={titleRef} />
-      <Form.Control type="text" placeholder="Description" ref={descRef} />
-      <Form.Control type="number" placeholder="Rating (0-10)" ref={ratingRef} />
-      <Form.Control type="text" placeholder="Poster URL" ref={posterRef} />
-      <Button onClick={handleAddMovie}>Add Movie</Button>
-    </Form>
+    
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Add new movie
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>New movie</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form className="add-movie-form">
+            <Form.Control type="text" placeholder="Title" ref={titleRef} />
+            <Form.Control type="text" placeholder="Description" ref={descRef} />
+            <Form.Control type="number" placeholder="Rating (0-10)" ref={ratingRef} />
+            <Form.Control type="text" placeholder="Poster URL" ref={posterRef} />
+        </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => {
+                handleAddMovie();
+                handleClose();
+            }}>
+            Add Movie
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+    
   );
 };
 
